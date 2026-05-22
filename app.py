93975f4946a409778f7651194cc3818f2f5b3ae2
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-22 20:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,14 +189,5 @@ for i in range(1, len(df)):
 
         <p>{str(e)}</p>
         """
-
 if __name__ == "__main__":
-
-    port = int(
-        os.environ.get("PORT", 8080)
-    )
-
-    app.run(
-        host="0.0.0.0",
-        port=port
-    )
+    app.run()
